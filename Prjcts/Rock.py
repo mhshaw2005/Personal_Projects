fd2552315345c9f73b769ccdbdc0a57cf497b179
Milo Shaw <mhshaw2005@gmail.com>
2025-02-17 13:36:02 -0700

    commit

--- Prjcts/Rock.py
+++ Prjcts/Rock.py
@@ -1,3 +1,5 @@
+import random
+
 workouts= {
     "chest" : ["incline press", "flat press", "pec fly's", "dips", "push-ups"],
     "shoulders" :["overhead press", "lateral raises", "pike push-ups"],
@@ -21,8 +23,11 @@ def _main():
     difficulty = input("On a scale of 1-10 pick your workout difficulty: ")
     diff_setting(int(difficulty))
 
-    num = int(input("How many different areas do you want to work out, up to 7: "))
-    groups = aresToWork(num)
+    num = int(input("How many different areas do you want to work out, up to 7 (0 for random): "))
+    if 0 < num < 8 :
+        makesets(aresToWork(num))
+    else:
+        makesets(aresToWork(random.randint(1,7),True))
 
 
 def diff_setting(difficulty):
@@ -50,26 +55,63 @@ def diff_setting(difficulty):
         print("Not a valid level. Try again!")
         _main()
 
-def aresToWork(num):
+
+def aresToWork(num, rand = False):
     alpha = []
+    prev = []
 
-    print("Options: Chest (1), Shoulders (2), Arms (3), Legs(4), Back (5), Abs(6), Cardio(7).")
-    print("Please do not enter the same group twice!")
-    for i in range(1,num + 1):
-        temp = int(input("Enter the number of the desired area: "))
-        if temp == 1:
-            alpha.append("chest")
-        elif temp == 2:
-            alpha.append("shoulders")
-        elif temp == 3:
-            alpha.append("arms")
-        elif temp == 4:
-            alpha.append("legs")
-        elif temp == 5:
-            alpha.append("back")
-        elif temp == 6:
-            alpha.append("abs")
-        elif temp == 7:
-            alpha.append("cardio")
+    if not rand:
+        print("Options: Chest (1), Shoulders (2), Arms (3), Legs(4), Back (5), Abs(6), Cardio(7).")
+        print("Please do not enter the same group twice!")
 
+        for i in range(1,num + 1):
+            temp = int(input("Enter the number of the desired area: "))
+            if temp == 1:
+                alpha.append("chest")
+            elif temp == 2:
+                alpha.append("shoulders")
+            elif temp == 3:
+                alpha.append("arms")
+            elif temp == 4:
+                alpha.append("legs")
+            elif temp == 5:
+                alpha.append("back")
+            elif temp == 6:
+                alpha.append("abs")
+            elif temp == 7:
+                alpha.append("cardio")
+            prev.append(temp)
+    elif rand:
+        for i in range(1,num + 1):
+            temp = random.randint(1,7)
+            if temp == 1:
+                alpha.append("chest")
+            elif temp == 2:
+                alpha.append("shoulders")
+            elif temp == 3:
+                alpha.append("arms")
+            elif temp == 4:
+                alpha.append("legs")
+            elif temp == 5:
+                alpha.append("back")
+            elif temp == 6:
+                alpha.append("abs")
+            elif temp == 7:
+                alpha.append("cardio")
+            prev.append(temp)
     return alpha
+
+
+def makesets(alpha):
+    print("")
+
+
+def randworkout(area):
+    exercises = []
+
+    theta = workouts.get(area)
+    beta = random.choice(theta)
+    exercises.append(beta)
+
+    return  exercises
+
